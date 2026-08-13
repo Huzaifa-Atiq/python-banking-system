ce9b911c8a781ddcd1b3b9f012958b94dd04ffbc
huzaifa-atiq <gganers123@gmail.com>
2026-08-13 19:54:00 +0500

    inital push files

---
+++ database.py
@@ -0,0 +1,119 @@
+import sqlite3
+import hashlib
+connect = sqlite3.connect("bank.db")
+cursor = connect.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS accounts (
+        accountId INTEGER PRIMARY KEY,
+        name TEXT,
+        balance REAL,
+        password TEXT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transactions (
+        transactionId INTEGER PRIMARY KEY,
+        accountId INTEGER,
+        type TEXT CHECK(type IN ('deposit', 'withdrawal')),
+        amount REAL
+    )
+""")
+
+connect.commit()
+
+def create_account():
+    while True:
+        username = input("Create your username: ")
+
+        cursor.execute(
+            "SELECT * FROM accounts WHERE name = ?",
+            (username,)
+        )
+
+        result = cursor.fetchone()
+
+        if result is None:
+            break
+
+        print("Username already exists!")
+
+    password = input("Create your password: ")
+
+
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    cursor.execute("""
+    INSERT INTO ACCOUNTS (name, balance, password)
+    VALUES(?, ?, ?)
+    """, (username, 0, hashed))
+
+    connect.commit()
+
+def login_user():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+
+    cursor.execute(
+        "SELECT * FROM accounts WHERE name = ? AND password = ?",
+        (username,hashed)
+    )
+
+    result = cursor.fetchone()
+
+    if result:
+        print("Login successful!")
+        return result[0]   # accountId
+    else:
+        print("Wrong username/password")
+        return None
+
+def change_balance(account_id):
+    opt = ""
+
+    while not (opt == "d" or opt == "w"):
+        opt = input("Type 'w' for withdrawal and 'd' for deposit: ").lower()
+
+    amount = float(input("Enter the amount: "))
+
+    if opt == "d":
+        cursor.execute(
+            "UPDATE accounts SET balance = balance + ? WHERE accountId = ?",
+            (amount, account_id)
+        )
+
+        cursor.execute(
+            "INSERT INTO transactions (account_id, type, amount) VALUES (?, ?, ?)",
+            (account_id, "deposit", amount)
+        )
+
+    elif opt == "w":
+        cursor.execute(
+            "SELECT balance FROM accounts WHERE accountId = ?",
+            (account_id,)
+        )
+
+        balance = cursor.fetchone()[0]
+
+        if amount > balance:
+            print("Insufficient balance.")
+            return
+
+        cursor.execute(
+            "UPDATE accounts SET balance = balance - ? WHERE accountId = ?",
+            (amount, account_id)
+        )
+
+        cursor.execute(
+            "INSERT INTO transactions (account_id, type, amount) VALUES (?, ?, ?)",
+            (account_id, "withdrawal", amount)
+        )
+
+    connect.commit()
+    print("Transaction successful!")
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,35 @@
+from database import create_account, login_user, change_balance
+
+print("--WELCOME TO THE BANK SYSTEM--")
+
+
+while True:
+    print("""
+    select an action:
+    1. Create account
+    2. login
+    """)
+
+    #select the option
+    option = input("Enter the number: ")
+
+    if option == "1":
+        create_account()
+
+    elif option == "2":
+        AccountID = login_user()
+        if AccountID:
+            
+            while True:
+                print("\nSelect an option: ")
+                print("1. logout")
+                print("2. deposit or withdraw")
+
+                selection = input("Enter your choice: ") 
+
+                if selection == "1":
+                    break
+                elif selection == "2":
+                    change_balance(AccountID)
+
+   
